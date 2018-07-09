1b98aeef129c562122a0b9a8e60e777d997f7034
Konstantin Domnitser <konstantin.domnitser@rms.com>
2018-07-09 07:43:36 -0700

    checkpoint before trying sgd

--- Linear_Classifier.py
+++ Linear_Classifier.py
@@ -33,25 +33,32 @@ class Linear_Classifier(object):
         self.loss_function = loss_function
 
     # todo: actual gradients based on backprop thru hinge loss and W
-    def gradient_descent(self, loss, rate, example_label):
-        loss_gradients = self.loss_function.backward(loss, example_label)
-        gradients = np.zeros((num_classes, example_length))/1000
+    # if you want to process a batch, you calculate the total loss for all samples
+    # but do you sum gradients as well? SVM gradient function is different if it's the labeled class or not
+    # also need to add biases, or use bias trick
+    def update(self, losses, rate, y):
+        gradients = np.zeros((num_classes, example_length))
+        loss_gradients = self.loss_function.backward(losses, y)
         return self.W + gradients * rate
 
+    # train on all samples
     def train(self, X, y, rate):
-        batch_scores = X.dot(self.W.T)
-        losses = np.zeros(batch_scores.shape[1])
+        batch_scores = X.dot(self.W.T)  # (num_samples, num_classes)
+        losses = np.zeros(batch_scores.shape[1])  # (num_classes,)
+        gradients = np.zeros_like(losses)  # (num_classes,)
         for example_scores, example_label in zip(batch_scores, y):
             example_loss = self.loss_function.forward(example_scores, example_label)
             losses += example_loss
-        self.W = self.gradient_descent(losses, rate, example_label)
+            example_gradients = np.zeros((num_classes, example_length))
+
+        self.W = self.update(losses, rate, y)
 
     def predict(self, X):
         scores = X.dot(self.W.T)
         return np.apply_along_axis(np.argmax, 1, scores)
 
 
-data = cifar.load('cifar-10', max_data_size=10, part_validation=0.1)
+data = cifar.load('cifar-10', max_data_size=100, part_validation=0.1)
 Xtr = data['Xtr']
 ytr = data['ytr']
 Xva = data['Xva']
